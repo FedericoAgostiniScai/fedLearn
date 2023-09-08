bd68c2388a1d5ca59a39f5bbe2415a4eb7cdac31
FedericoAgostiniScai <federico.agostini@grupposcai.it>
2023-09-08 14:37:02 +0200

    llr

---
+++ llr/client.py
@@ -0,0 +1,56 @@
+import warnings
+from argparse import ArgumentParser
+import flwr as fl
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import log_loss
+
+import utils
+
+
+class myClient(fl.client.NumPyClient):
+    def __init__(self, cid, random_state=0):
+        data = np.load(f"../data/train_{cid}.npz")
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            data["X"], data["y"], test_size=0.15, random_state=random_state)
+        self.model = utils.get_sklearn_model(client=True)
+        utils.set_initial_params(self.model)
+
+    def get_parameters(self, config):  # type: ignore
+        return utils.get_model_parameters(self.model)
+
+    def fit(self, parameters, config):  # type: ignore
+        utils.set_model_params(self.model, parameters)
+        # Ignore convergence failure due to low local epochs
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self.model.fit(self.X_train, self.y_train)
+        print(f"Training finished for round {config['server_round']}")
+        return utils.get_model_parameters(self.model), len(self.X_train), {}
+
+    def evaluate(self, parameters, config):  # type: ignore
+        utils.set_model_params(self.model, parameters)
+        loss = log_loss(self.y_test, self.model.predict_proba(self.X_test))
+        accuracy = self.model.score(self.X_test, self.y_test)
+        print(f"Loss = {loss:.4} | acc = {accuracy:.4}")
+        return loss, len(self.X_test), {"accuracy": accuracy}
+# %%
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--id", type=int, help="Client ID")
+
+    return (vars(parser.parse_args()))
+
+# %%
+
+
+if __name__ == "__main__":
+
+    args = parse_args()
+
+    # Start Flower client
+    fl.client.start_numpy_client(
+        server_address="0.0.0.0:8080", client=myClient(cid=args["id"]))
---
+++ llr/utils.py
@@ -0,0 +1,54 @@
+# %%
+
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+
+
+def get_model_parameters(model):
+    """Returns the paramters of a sklearn LogisticRegression model."""
+    if model.fit_intercept:
+        params = [
+            model.coef_,
+            model.intercept_,
+        ]
+    else:
+        params = [
+            model.coef_,
+        ]
+    return params
+
+
+def set_model_params(model, params):
+    """Sets the parameters of a sklean LogisticRegression model."""
+    model.coef_ = params[0]
+    if model.fit_intercept:
+        model.intercept_ = params[1]
+    return model
+
+
+def set_initial_params(model):
+    """Sets initial parameters as zeros Required since model params are uninitialized
+    until model.fit is called.
+
+    But server asks for initial parameters from clients at launch. Refer to
+    sklearn.linear_model.LogisticRegression documentation for more information.
+    """
+    n_classes = 10  # MNIST has 10 classes
+    n_features = 784  # Number of features in dataset
+    model.classes_ = np.array([i for i in range(10)])
+
+    model.coef_ = np.zeros((n_classes, n_features))
+    if model.fit_intercept:
+        model.intercept_ = np.zeros((n_classes,))
+
+
+# %%
+
+
+def get_sklearn_model(client=False):
+    params = {}
+    if client:
+        params.update(
+            {"max_iter": 1, "warm_start": True})
+    model = LogisticRegression(**params)
+    return model
